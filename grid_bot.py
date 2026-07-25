7c9600cb869ad6bd1d470b25254a2eb129e208d4
cromp3-cmyk <cromp3@gmx.de>
2026-07-26 01:23:21 +0200

    Update grid_bot.py

--- grid_bot.py
+++ grid_bot.py
@@ -184,28 +184,15 @@ def calc_unrealized_pnl(symbol):
     else:
         return round((st["avg_entry_price"] - st["last_price"]) * st["total_coin_size"], 4)
 
-
-        
 async def fetch_candles_for_psar(symbol, resolution, count_back=100):
     import lighter
     configuration = lighter.Configuration(host=BASE_URL)
     async with lighter.ApiClient(configuration) as api_client:
         candle_api = lighter.CandlestickApi(api_client)
-        now_ms = int(time.time() * 1000)
-        # Berechne Startzeit basierend auf count_back und resolution
-        resolution_seconds = {
-            "1": 60, "2": 120, "5": 300, "10": 600,
-            "15": 900, "30": 1800, "60": 3600,
-            "240": 14400
-        }
-        seconds_per_candle = resolution_seconds.get(str(resolution), 300)
-        start_ms = now_ms - (count_back * seconds_per_candle * 1000)
-        
         response = await candle_api.candles(
             market_id=MARKET_INDICES[symbol],
             resolution=resolution,
-            start_timestamp=start_ms,
-            end_timestamp=now_ms,
+            count_back=min(count_back, 500),
             set_timestamp_to_end=True,
         )
         candles = getattr(response, "c", None)
